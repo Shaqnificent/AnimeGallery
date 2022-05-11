d598e40a5eedee69094ce40ba12b9a0707bb6a4b
Shaqnificent <shaqnificent@gmail.com>
2022-05-10 19:09:45 -0500

    Added Search bar to nav bar

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,12 +1,16 @@
 <template lang="html">
-    <div class="p-3 flex items-center justify-between">
+    <div class="px-3 py-4 flex items-center justify-between">
         <div class="flex items-center">
             <button class="rounded-full hover:bg-light p-3 flex items-center justify-center" type="button" name="button">
                 <img src="logo.png" alt="" class="h-6 w-6 rounded-full">
             </button>
-            <button v-for="(item, i) in tabItems" :key="i" @click="selectedTab = item.key" :class="`${selectedTab=== item.key ? 'bg-dark text-white': 'text-dark hover:bg-light font-semibold'} rounded-full py-3 px-5`" type="button" name="button">
+            <button v-for="(item, i) in tabItems" :key="i" @click="selectedTab = item.key" :class="`${selectedTab=== item.key ? 'bg-dark text-white': 'text-dark hover:bg-light font-semibold'} rounded-full py-3 px-4`" type="button" name="button">
                 {{item.name}}
             </button>
+        </div>
+        <div class="flex-grow relative">
+            <span class="material-icons absolute mt-3 ml-3 text-muted">search</span>
+            <input class="hover:bg-lightHover w-full rounded-full bg-light py-3 pr-3 pl-10" placeholder="Search" type="text" name="" values="">
         </div>
     </div>
 </template>
