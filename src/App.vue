82397aca1baf6a200e11d90ba611b1dd879a4b46
Shaqnificent <shaqnificent@gmail.com>
2022-05-12 10:45:57 -0500

    Added card item

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <top-nav/>
+  <div class="px-20">
+    <div class="w-1/5">
+    <CardItem src="1.jpg"  class="w-full h-full" />
+  </div>
+  </div>
+  
 </template>
 
 <script>
 import TopNav from './components/TopNav.vue';
+import CardItem from './components/CardItem.vue';
+
 export default {
   name: 'App',
   components: {
-    TopNav
+    TopNav,
+    CardItem
   }
 }
 </script>
---
+++ src/components/CardItem.vue
@@ -0,0 +1,38 @@
+<template lang="html">
+    <div @mouseover="hover = true" @mouseleave="hover=false" class="relative">
+        <img :src="src" alt="" class="rounded-lg">
+        <div v-if="hover" class="absolute bg-dark opacity-50 w-full inset-0 rounded-lg">
+        </div> 
+        <div v-if="hover"  class="absolute top-0 w-full h-full flex flex-col justify-between p-3">
+            <div class=" flex items-center justify-between">
+                <button type="button" name="button" class="flex items-center">
+                    <p class="font-semibold text-white">UI</p>
+                    <span class="material-icons text-white">expand_more</span> 
+                </button>
+                <button type="button" name="button" class="rounded-full py-3 px-5 text-white bg-primary font-semibold">
+                    Save
+                </button>
+            </div>
+             <div class="flex items-center w-full">
+                    <button name="button" type="button" class="bg-white mr-2 ml-auto rounded-full w-8 h-8 flex items-center justify-center text-dark opacity-80 hover:opacity-100">
+                        <span class="material-icons text-base">ios_share</span>
+                    </button>
+                     <button name="button" type="button" class="bg-white rounded-full w-8 h-8 flex items-center justify-center p-2 text-dark opacity-80 hover:opacity-100">
+                        <span class="material-icons text-base">more_horiz</span>
+                    </button>
+                </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['src'],
+    data() {
+        return {
+            hover: false
+        }
+    }
+}
+</script>
+
