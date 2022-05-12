49d2392787842cd7b0fe60d6f620e7821b5fab94
Shaqnificent <shaqnificent@gmail.com>
2022-05-12 12:20:06 -0500

    added some photos

--- src/App.vue
+++ src/App.vue
@@ -72,124 +72,124 @@ export default {
           recommended: 'Naruto'
         },
                 {
-          src: '1.jpg',
+          src: '6.jpg',
           title: 'Title 1',
           user: 'User 1',
           recommended: 'Naruto'
         },
                 {
-          src: '2.jpg',
+          src: '7.png',
           title: 'Title 2',
           user: 'User 2',
           recommended: 'Naruto'
         },
                 {
-          src: '3.jpg',
+          src: '8.jpg',
           title: 'Title 3',
           user: 'User 3',
-          recommended: 'Naruto'
+          recommended: 'DBZ'
         },
                 {
-          src: '4.png',
+          src: '9.png',
           title: 'Title 4',
           user: 'User 4',
-          recommended: 'Naruto'
+          recommended: 'DBZ'
         },
                 {
-          src: '5.jpg',
+          src: '10.jpg',
           title: 'Title 5',
           user: 'User 5',
-          recommended: 'Naruto'
+          recommended: 'DBZ'
         },
                 {
-          src: '1.jpg',
-          title: 'Title 1',
-          user: 'User 1',
-          recommended: 'Naruto'
+          src: '11.jpg',
+          title: 'Title 11',
+          user: 'User 11',
+          recommended: 'DBZ'
         },
                 {
-          src: '2.jpg',
-          title: 'Title 2',
-          user: 'User 2',
-          recommended: 'Naruto'
+          src: '12.jpg',
+          title: 'Title 12',
+          user: 'User 12',
+          recommended: 'Haikyuu'
         },
                 {
-          src: '3.jpg',
-          title: 'Title 3',
-          user: 'User 3',
-          recommended: 'Naruto'
+          src: '13.jpg',
+          title: 'Title 13',
+          user: 'User 13',
+          recommended: 'Haikyuu'
         },
                 {
-          src: '4.png',
-          title: 'Title 4',
-          user: 'User 4',
-          recommended: 'Naruto'
+          src: '14.png',
+          title: 'Title 14',
+          user: 'User 14',
+          recommended: 'Haikyuu'
         },
                 {
-          src: '5.jpg',
-          title: 'Title 5',
-          user: 'User 5',
-          recommended: 'Naruto'
+          src: '15.jpg',
+          title: 'Title 15',
+          user: 'User 15',
+          recommended: 'Bleach'
         },
                 {
-          src: '1.jpg',
-          title: 'Title 1',
-          user: 'User 1',
-          recommended: 'Naruto'
+          src: '16.jpg',
+          title: 'Title 16',
+          user: 'User 16',
+          recommended: 'Bleach'
         },
                 {
-          src: '2.jpg',
-          title: 'Title 2',
-          user: 'User 2',
-          recommended: 'Naruto'
+          src: '17.png',
+          title: 'Title 17',
+          user: 'User 17',
+          recommended: 'Bleach'
         },
                 {
-          src: '3.jpg',
-          title: 'Title 3',
-          user: 'User 3',
-          recommended: 'Naruto'
+          src: '18.jpg',
+          title: 'Title 18',
+          user: 'User 18',
+          recommended: 'Bleach'
         },
                 {
-          src: '4.png',
-          title: 'Title 4',
-          user: 'User 4',
-          recommended: 'Naruto'
+          src: '19.png',
+          title: 'Title 19',
+          user: 'User 19',
+          recommended: 'Bleach'
         },
                 {
-          src: '5.jpg',
-          title: 'Title 5',
-          user: 'User 5',
-          recommended: 'Naruto'
+          src: '20.jpg',
+          title: 'Title 20',
+          user: 'User 20',
+          recommended: 'JJK'
         },
                 {
-          src: '1.jpg',
-          title: 'Title 1',
-          user: 'User 1',
-          recommended: 'Naruto'
+          src: '21.jpg',
+          title: 'Title 21',
+          user: 'User 21',
+          recommended: 'Black Clover'
         },
                 {
-          src: '2.jpg',
-          title: 'Title 2',
-          user: 'User 2',
-          recommended: 'Naruto'
+          src: '22.jpg',
+          title: 'Title 22',
+          user: 'User 22',
+          recommended: 'Black clover'
         },
                 {
-          src: '3.jpg',
-          title: 'Title 3',
-          user: 'User 3',
-          recommended: 'Naruto'
+          src: '23.jpg',
+          title: 'Title 23',
+          user: 'User 23',
+          recommended: 'Fairy Tail'
         },
                 {
-          src: '4.png',
-          title: 'Title 4',
-          user: 'User 4',
-          recommended: 'Naruto'
+          src: '24.png',
+          title: 'Title 24',
+          user: 'User 24',
+          recommended: 'Fairy Tail'
         },
                 {
-          src: '5.jpg',
-          title: 'Title 5',
-          user: 'User 5',
-          recommended: 'Naruto'
+          src: '25.jpg',
+          title: 'Title 25',
+          user: 'User 25',
+          recommended: 'Demon Slayer'
         }
       ]
     }
